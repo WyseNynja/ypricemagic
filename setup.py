f55ee7505e2e3939da72fe62ab2838e9f9bf29b8
Bryan Stitt <bryan@stitthappens.com>
2023-03-16 17:46:30 -0700

    use pip-compile and YPRICEMAGIC_LIB to loosen deps

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+from os import environ
 from setuptools import find_packages, setup
 
-with open("requirements.txt", "r") as f:
+if environ.get("YPRICEMAGIC_LIB", "0") == "1":
+    requirements_filename = "requirements.in"
+else:
+    requirements_filename = "requirements.txt"
+
+with open(requirements_filename, "r") as f:
     requirements = list(map(str.strip, f.read().split("\n")))[:-1]
 
 setup(
-    name='ypricemagic',
+    name="ypricemagic",
     packages=find_packages(),
     use_scm_version={
         "root": ".",
@@ -12,13 +18,13 @@ setup(
         "local_scheme": "no-local-version",
         "version_scheme": "python-simplified-semver",
     },
-    description='Use this tool to extract historical on-chain price data from an archive node. Shoutout to @bantg and @nymmrx for their awesome work on yearn-exporter that made this library possible.',
-    author='BobTheBuidler',
-    author_email='bobthebuidlerdefi@gmail.com',
-    url='https://github.com/BobTheBuidler/ypricemagic',
-    license='MIT',
+    description="Use this tool to extract historical on-chain price data from an archive node. Shoutout to @bantg and @nymmrx for their awesome work on yearn-exporter that made this library possible.",
+    author="BobTheBuidler",
+    author_email="bobthebuidlerdefi@gmail.com",
+    url="https://github.com/BobTheBuidler/ypricemagic",
+    license="MIT",
     install_requires=requirements,
     setup_requires=[
-        'setuptools_scm',
+        "setuptools_scm",
     ],
 )
